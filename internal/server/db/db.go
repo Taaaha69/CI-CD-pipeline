d419961bbb1b6c96e23625cf9ca6555559ec6682
Burak <burakaksoy516@gmail.com>
2026-02-04 17:35:17 +0300

    fix db.go

--- internal/server/db/db.go
+++ internal/server/db/db.go
@@ -9,6 +9,9 @@ import (
 
 func Connect(path string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
 
 	if err := db.Ping(); err != nil {
 		return nil, err
