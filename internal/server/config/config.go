cbc3b2144e24a6c78f70bf4971e2b2d411834ef9
Burak <burakaksoy516@gmail.com>
2026-02-04 17:33:29 +0300

    fix config.go

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -20,5 +20,13 @@ func Load(path string) (*Config, error) {
 
 	file, err := os.ReadFile(path)
 
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(file, cfg); err != nil {
+		return nil, err
+	}
+
 	return cfg, nil
 }
